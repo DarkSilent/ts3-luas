c0891846e9c36fca696bc29819832b52be3638e5
Jean- Michael Bergmann <michael@evildev.de>
2013-06-24 04:51:45 +0300

    Add Requirements in init.lua

--- defuse/init.lua
+++ defuse/init.lua
@@ -3,3 +3,10 @@ Game - Defuse the Bomb
 Developer: Jean- Michael Bergmann (Original by Lil-rich @2009)
 ]]--
 
+-- Requirements --
+require("ts3init")
+require("ts3defs")
+require("ts3errors")
+require("defuse/events")
+require("defuse/functions")
+
